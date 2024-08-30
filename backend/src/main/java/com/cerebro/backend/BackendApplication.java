a9e3fc764a0e14791718208f83f2a3cb437fff09
Fabio Pelagaggi <fabio.pnunes@al.infnet.edu.br>
2024-08-29 22:49:04 -0300

    remove eureka

--- backend/src/main/java/com/cerebro/backend/BackendApplication.java
+++ backend/src/main/java/com/cerebro/backend/BackendApplication.java
@@ -2,16 +2,13 @@ package com.cerebro.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.http.EurekaApplications;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
+
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
